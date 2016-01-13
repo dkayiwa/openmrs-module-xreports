aec2e00e06ff8ea14e109094c534e5c684fbf346
Daniel Kayiwa <kayiwadaniel@gmail.com>
2016-01-13 23:07:55 +0300

    Linking the xreport designer to the reporting module

--- omod/src/main/java/org/openmrs/module/xreports/web/controller/ReportDesignerController.java
+++ omod/src/main/java/org/openmrs/module/xreports/web/controller/ReportDesignerController.java
@@ -1,9 +1,16 @@
 package org.openmrs.module.xreports.web.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.reporting.report.service.ReportService;
+import org.openmrs.module.xreports.XReport;
 import org.openmrs.module.xreports.api.XReportsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -32,6 +39,33 @@ public class ReportDesignerController {
 		map.put("undoRedoBufferSize", service.getGlobalProperty("xreports.undoRedoBufferSize", "100"));
 		
 		boolean refapp = "true".equals(request.getParameter("refApp"));
-		map.put("closeUrl", request.getContextPath() + (refapp ? "/xreports/reports.page" : "/module/xreports/report.form") + "?reportId=" + reportId);
+		String url = (refapp ? "/xreports/reports.page" : "/module/xreports/report.form") + "?reportId=" + reportId;
+		String returnUrl = request.getParameter("returnUrl");
+		if (StringUtils.isNotBlank(returnUrl)) {
+			url = returnUrl;
+		}
+		map.put("closeUrl", request.getContextPath() + url);
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/module/reporting/reports/renderers/defaultReportDesignEditor.htm")
+	public String displayReportDesigner(HttpServletRequest request) {
+		
+		String parameters = request.getParameter("parameters");
+		String reportDesignUuid = parameters.replace("reportDesignUuid=", "");
+		reportDesignUuid = reportDesignUuid.replace("|", "");
+		
+		ReportService rs = Context.getService(ReportService.class);
+		ReportDesign design = rs.getReportDesignByUuid(reportDesignUuid);
+		ReportDefinition reportDef = design.getReportDefinition();
+		XReportsService service = Context.getService(XReportsService.class);
+		List<XReport> reports = service.getReportsByExternalUuid(reportDef.getUuid());
+		if (reports != null && reports.size() > 0) {
+			return "redirect:/module/xreports/reportDesigner.form?reportId=" + reports.get(0).getReportId() + 
+					"&parameters=" + parameters + 
+					"&returnUrl=/module/reporting/reports/manageReportDesigns.form";
+		}
+		
+		return null;
 	}
 }
